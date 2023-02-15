5ad34ce3564bdb8a707e743f547d2676e6fe909b
samali-rayen <115874034+samali-rayen@users.noreply.github.com>
2023-02-15 16:54:07 +0100

    correction of the code

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 let start=document.getElementById("on-off")
 let del=document.getElementById("deleteButton")
-let result
+let result=document.getElementById("result")
+let equation=""
 let isON=false
 start.addEventListener("click",function() {
     if(isON===false){
-        document.getElementById("result").value="00000000000000000000"
+        result.value="00000000000000000000"
         isON=true
     setTimeout(function(){
-        document.getElementById("result").value=""
+        result.value=""
     },100)
     }else{
-        document.getElementById("result").value=""
+        result.value=""
         isON=false
     }
 })//the setTimeout function is used to simulate turning on and off the calculator
 del.addEventListener("click",function(){
-
+    if(isON){
+        result.value=""
+        equation="" 
+    }
+   
 })
+function updateDisplay(value){
+    if(isON){
+        equation+=value
+        result.value=equation   
+    }
+    
+}
+function equal(){
+    if(isON){
+        try{
+        let answer=eval(equation);
+        result.value=answer;
+        equation="";
+    }catch(error){
+        alert('Invalid equation');
+    }  
+    }
+
+}
