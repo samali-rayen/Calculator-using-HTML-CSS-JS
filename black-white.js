4ef791c74f91cef9c5afb946073e0e9602b71584
samali-rayen <115874034+samali-rayen@users.noreply.github.com>
2023-02-18 02:17:05 +0100

    still trying for the changment of he theme

--- black-white.js
+++ black-white.js
@@ -1,12 +1,1 @@
-const change=document.getElementById("switch");
-const bod=document.querySelector("body");
-change.addEventListener('click',function(){
-    this.classList.change='bi bi-moon'
-    if( this.classList.change='bi bi-brightness-low'){
-        bod.style.background='linear-gradient(#544a7d,#ffd452)'
-        bod.style.transition='2s'
-    }else{
-        bod.style.background='radial-gradient(circle, rgba(105,61,16,1) 0%, rgba(26,27,23,1) 100%)'
-        bod.style.transition='2s';
-    }
-})
+
--- index.js
+++ index.js
@@ -40,3 +40,11 @@ function equal(){
     }  
     }
 }
+
+// for the theme changement--------------------------------
+let content=document.getElementsByTagName('body')[0];
+let darkMode=document.getElementsById('dark-change');
+darkMode.addEventListener('click',function(){
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night');
+})
